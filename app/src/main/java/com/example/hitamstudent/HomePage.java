915226a1c5c56a99411709bf205d4b086d8e17d3
AnanyaGain <ananyagain4@gmail.com>
2022-10-24 22:26:50 +0530

    navigation bar and drawer applied

--- app/src/main/java/com/example/hitamstudent/HomePage.java
+++ app/src/main/java/com/example/hitamstudent/HomePage.java
@@ -1,14 +1,89 @@
 package com.example.hitamstudent;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class HomePage extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
+import java.util.Objects;
+
+public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        navController = Navigation.findNavController(this,R.id.frame_layout);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigation_view);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout,R.string.start, R.string.close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.navigation_assignments:
+                Toast.makeText(this,"Assignments", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_attendance:
+                Toast.makeText(this,"Attendance", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_syllabus:
+                Toast.makeText(this,"Syllabus", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_calendar:
+                Toast.makeText(this,"Academic Calendar", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_library:
+                Toast.makeText(this,"Library Books / e-Books", Toast.LENGTH_SHORT).show();
+                break;
+       }
+        return true;
     }
 }
